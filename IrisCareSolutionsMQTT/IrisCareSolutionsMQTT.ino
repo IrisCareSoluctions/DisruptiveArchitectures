f234b2090043c88382446b6dab7883952b37ece5
Rafaela Gomes <99452621+gsrafaela@users.noreply.github.com>
2023-11-28 00:04:50 -0300

    Update IrisCareSolutionsMQTT.ino

--- IrisCareSolutionsMQTT/IrisCareSolutionsMQTT.ino
+++ IrisCareSolutionsMQTT/IrisCareSolutionsMQTT.ino
@@ -1,132 +1,113 @@
 #include <PubSubClient.h>
 #include <WiFi.h>
 
-const char *ssid = "SuaRedeWiFi";  // Coloque o nome da sua rede WiFi
-const char *password = "SuaSenhaWiFi";  // Coloque a senha da sua rede WiFi
-const char *mqtt_server = "broker.mqtt.com";  // Coloque o endereço do seu servidor MQTT
+const char* ssid = "Vilma";
+const char* password = "mamis6350";
+const char* mqttServer = "SEU_BROKER_MQTT";
+const int mqttPort = 1883;
+const char* mqttUser = "SEU_USUARIO_MQTT";
+const char* mqttPassword = "SUA_SENHA_MQTT";
 
-const char *topic = "sensor/ultrassom";
+#define PINO_LED9 9
+#define SENSOR 10
+#define DELAY 50
+#define QUANT_MAX 6
+#define TEMPO_PISCA 200
+
+bool VALOR_SENSOR = LOW;
+int CONTADOR = 0;
+bool SETA_SENSOR = LOW;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-const int pinoBuzzer = 2; // PINO DIGITAL EM QUE O BUZZER ESTÁ CONECTADO
-int inches = 0;
-int cm = 0;
-
-long readUltrasonicDistance(int triggerPin, int echoPin)
-{
-  pinMode(triggerPin, OUTPUT);  // Clear the trigger
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigger pin to HIGH state for 10 microseconds
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
-  pinMode(echoPin, INPUT);
-  // Reads the echo pin, and returns the sound wave travel time in microseconds
-  return pulseIn(echoPin, HIGH);
-}
-
-void setup_wifi()
-{
-  delay(10);
-  // Conectar-se a uma rede WiFi
-  Serial.println();
-  Serial.print("Conectando a ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi conectado");
-  Serial.println("Endereço IP: ");
-  Serial.println(WiFi.localIP());
-}
-
-void callback(char *topic, byte *payload, unsigned int length)
-{
-  Serial.print("Mensagem recebida no tópico: ");
-  Serial.println(topic);
-
-  Serial.print("Mensagem:");
-  for (int i = 0; i < length; i++)
-  {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-}
-
-void reconnect()
-{
-  // Loop até que estejamos reconectados
-  while (!client.connected())
-  {
-    Serial.print("Tentando se reconectar ao MQTT...");
-
-    // Tente se reconectar
-    if (client.connect("arduinoClient"))
-    {
-      Serial.println("Conectado!");
-      // Assine o tópico
-      client.subscribe(topic);
-    }
-    else
-    {
-      Serial.print("Falha, rc=");
-      Serial.print(client.state());
-      Serial.println(" Tente novamente em 5 segundos");
-      // Aguarde 5 segundos antes de tentar novamente
-      delay(5000);
-    }
-  }
-}
-
-void setup()
-{
+void setup() {
+  pinMode(PINO_LED9, OUTPUT);
+  pinMode(SENSOR, INPUT);
   Serial.begin(9600);
 
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando ao WiFi...");
+  }
+  Serial.println("Conectado ao WiFi");
+
+  client.setServer(mqttServer, mqttPort);
   client.setCallback(callback);
 }
 
-void loop()
-{
-  if (!client.connected())
-  {
+void loop() {
+  if (!client.connected()) {
     reconnect();
   }
   client.loop();
 
-  // measure the ping time in cm
-  cm = 0.01723 * readUltrasonicDistance(5, 7);
-  // convert to inches by dividing by 2.54
-  inches = (cm / 2.54);
-  Serial.print(inches);
-  Serial.print("in, ");
-  Serial.print(cm);
-  Serial.println("cm");
+  VALOR_SENSOR = digitalRead(SENSOR);
+  delay(DELAY);
+  digitalWrite(PINO_LED9, !VALOR_SENSOR);
 
-  // Publica a leitura do sensor no tópico MQTT
-  char msg[10];
-  sprintf(msg, "%d", cm);
-  client.publish(topic, msg);
-
-  delay(1000); // Aguarde 1 segundo(s)
-
-  if (cm <= 50)
-  { // SE A DISTÂNCIA ENTRE O OBJETO E O SENSOR ULTRASSÔNICO FOR MENOR QUE 50CM, FAZ
-    tone(pinoBuzzer, 1500); // ACIONA O BUZZER
+  if (VALOR_SENSOR == LOW) {
+    SETA_SENSOR = HIGH;
   }
-  else
-  { //SENÃO, FAZ
-    noTone(pinoBuzzer); //BUZZER PERMANECE DESLIGADO
+  if (VALOR_SENSOR == HIGH && SETA_SENSOR == HIGH) {
+    CONTADOR = CONTADOR + 1;
+    SETA_SENSOR = LOW;
+    Serial.print("QUANTIDADE DE PEÇAS=");
+    Serial.println(CONTADOR);
+
+    // Publicar o valor do contador no tópico "contador"
+    client.publish("contador", String(CONTADOR).c_str());
+  }
+
+  if (CONTADOR >= QUANT_MAX) {
+    client.publish("status", "Quantidade máxima atingida");
+    digitalWrite(PINO_LED9, HIGH);
+    delay(TEMPO_PISCA);
+    digitalWrite(PINO_LED9, LOW);
+    delay(TEMPO_PISCA);
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Mensagem recebida no tópico: ");
+  Serial.println(topic);
+
+  String message = "";
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  Serial.print("Conteúdo da mensagem: ");
+  Serial.println(message);
+
+  // Lógica de controle remoto
+  if (strcmp(topic, "controle") == 0) {
+    if (message == "LIGAR_LED") {
+      digitalWrite(PINO_LED9, HIGH);
+      Serial.println("LED ligado");
+    } else if (message == "DESLIGAR_LED") {
+      digitalWrite(PINO_LED9, LOW);
+      Serial.println("LED desligado");
+    } else if (message == "ZERAR_CONTADOR") {
+      CONTADOR = 0;
+      Serial.println("Contador zerado");
+    }
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.println("Tentando se reconectar ao MQTT...");
+
+    if (client.connect("ArduinoClient", mqttUser, mqttPassword)) {
+      Serial.println("Conectado ao broker MQTT");
+      // Assinar os tópicos relevantes aqui
+      client.subscribe("controle");
+    } else {
+      Serial.print("Falha na conexão ao broker, rc=");
+      Serial.print(client.state());
+      Serial.println(" Tentando novamente em 5 segundos");
+      delay(5000);
+    }
   }
 }
